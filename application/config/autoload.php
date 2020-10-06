b656da034616445e795c6e54609ec2bb76c7fbfd
alfan06 <alfannoufal@gmail.com>
2020-10-06 21:43:17 +0700

    repair img

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database', 'form_validation', 'session' ,'upload');
 
 /*
 | -------------------------------------------------------------------
@@ -92,7 +92,7 @@ $autoload['drivers'] = array();
 /*
 coba
 */
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url','form');
 
 /*
 | -------------------------------------------------------------------
